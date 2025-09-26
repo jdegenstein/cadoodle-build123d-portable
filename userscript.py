22a74d04c4984ba472da19ec26db64a493a197d9
jdegenstein <jdegenstein@users.noreply.github.com>
2025-09-26 15:44:37 -0500

    some modifications to the cadexport package

--- userscript.py
+++ userscript.py
@@ -3,6 +3,7 @@ An example script defining a build123d object.
 This file will be executed by the cadexport tool.
 """
 from build123d import *
+from ocp_vscode import *
 
 # The variable 'uservar' will be targeted by the command-line tool for export.
 # It's a simple box with a hole through it.
@@ -15,3 +16,4 @@ some_other_part = Sphere(30)
 
 # A simple text message to show that the script is being executed.
 print("userscript.py has been successfully executed.")
+show(uservar)
