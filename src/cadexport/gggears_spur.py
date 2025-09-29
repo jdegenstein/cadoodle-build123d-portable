a7f674d8810861a5b5ee08acf10f081b166030c5
jdegenstein <jdegenstein@users.noreply.github.com>
2025-09-29 15:38:16 -0500

    gggears_spur.py -

--- src/cadexport/gggears_spur.py
+++ src/cadexport/gggears_spur.py
@@ -1,8 +1,18 @@
 from gggears import *
 from build123d import *
 import fire
+from os import PathLike
 
-SpurGear.export_stl = export_stl
+def _export_stl(
+    self,
+    file_path: PathLike | str | bytes,
+    tolerance: float = 1e-3,
+    angular_tolerance: float = 0.1,
+    ascii_format: bool = False,
+) -> bool:
+    export_stl(self.build_part(), file_path, tolerance, angular_tolerance, ascii_format)
+    
+SpurGear.export_stl = _export_stl
 
 def main():
     fire.Fire(SpurGear)
